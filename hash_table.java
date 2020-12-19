76c68b8c7cb6c1c4e6df865d54abf653a6eb1b07
BlitzYp <63169496+BlitzYp@users.noreply.github.com>
2020-12-19 23:43:34 +0200

    Update hash_table.java

--- hash_table.java
+++ hash_table.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Dict {
     ArrayList<Integer> data = new ArrayList<>();
     int size = 0;
+    ArrayList<String> keys = new ArrayList<>();
 
     private int hash(String key) {
         int hash = 0;
@@ -18,11 +19,16 @@ public class Dict {
         size++;
         int hashedKey = hash(key);
         data.add(hashedKey, value);
+        keys.add(key);
     }
 
     public int get(String key) {
         int res = data.get(hash(key));
         System.out.println(res);
         return res;
+    }
+
+    public ArrayList<String> getKeys() {
+        return keys;
     }
 }
