1f8c8ad47546bab3712a7f04da9c8cb826d99849
BlitzYp <63169496+BlitzYp@users.noreply.github.com>
2021-04-21 12:41:52 +0300

    Update hash_table.java

--- hash_table.java
+++ hash_table.java
@@ -1,34 +1,62 @@
-import java.util.*;
+class Node {
+    public String key;
+    public String value;
+    public Node next;
+    public Node(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
 
-public class Dict {
-    ArrayList<Integer> data = new ArrayList<>();
-    int size = 0;
-    ArrayList<String> keys = new ArrayList<>();
+class hashTable {
+    public int maxSize;
+    public Node[] data;
+    public hashTable(int maxSize) {
+        this.maxSize = maxSize;
+        data = new Node[maxSize];
+    }
 
     private int hash(String key) {
-        int hash = 0;
-
+        int res = 0;
         for (int i = 0; i < key.length(); i++) {
-            hash = (hash + key.charAt(i)) % size;
+            res = res + (key.charAt(i) * i);
         }
-
-        return hash;
+        return res % maxSize;
     }
 
-    public void add(String key, int value) {
-        size++;
-        int hashedKey = hash(key);
-        data.add(hashedKey, value);
-        keys.add(key);
+    public void add(String key, String value) {
+        int index = hash(key);
+        Node newNode = new Node(key, value);
+        newNode.next = data[index];
+        data[index] = newNode;
     }
 
-    public int get(String key) {
-        int res = data.get(hash(key));
-        System.out.println(res);
-        return res;
+    public String get(String key) {
+        int index = hash(key);
+        Node res = data[index];
+        while (res.key != key && res.next != null) res = res.next;
+        return res.value;
     }
 
-    public ArrayList<String> getKeys() {
-        return keys;
+    public void keys() {
+        for (int i = 0; i < maxSize; i++) {
+            Node curr = data[i];
+            if (curr == null) continue;
+            while (curr != null) {
+                System.out.println(curr.key);
+                curr = curr.next;
+            }
+        }
+    }
+
+    public void values() {
+        for (int i = 0; i < maxSize; i++) {
+            Node curr = data[i];
+            if (curr == null) continue;
+            while (curr != null) {
+                System.out.println(curr.value);
+                curr = curr.next;
+            }
+        }
     }
 }
